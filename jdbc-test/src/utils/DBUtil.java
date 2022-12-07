2b36b0cc5097d6439dcc51b799a4ec1f76f985cf
githubXWJ <2933516492@qq.com>
2022-12-07 10:41:36 +0800

    jdbc2

---
+++ jdbc-test/src/utils/DBUtil.java
@@ -0,0 +1,58 @@
+package utils;
+
+import java.sql.*;
+
+/**
+ * @date 2022/10/27 - 20:50
+ * JDBC工具类, 简化JDBC编程
+ */
+public class DBUtil {
+
+    private DBUtil(){
+    }
+
+    static {//静态代码块在类加载时执行，并且只执行一次
+        try {
+            Class.forName("com.mysql.jdbc.Driver");//注册驱动
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static Connection getConnection() throws SQLException{
+          Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/bjpowernode","root","150798777278x");
+          return conn;
+    }
+
+    /**
+     * 关闭资源
+     * @param conn 连接对象
+     * @param ps  数据库操作对象
+     * @param rs 结果集
+     */
+    public static void close(Connection conn, Statement ps, ResultSet rs){
+        if (rs !=null){
+            try {
+                rs.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+        if (ps != null) {
+            try {
+                ps.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+
+    }
+}
